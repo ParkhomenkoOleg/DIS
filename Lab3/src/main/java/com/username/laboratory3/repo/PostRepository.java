d72a0e7a576d094f0e977b2c849029bd4bbe3ce5
ParkhomenkoOleg <parhomenkooleg37@gmail.com>
2022-11-19 16:37:51 +0200

    add folder with lab3

---
+++ Lab3/src/main/java/com/username/laboratory3/repo/PostRepository.java
@@ -0,0 +1,8 @@
+package com.username.laboratory3.repo;
+
+import com.username.laboratory3.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+
+}
